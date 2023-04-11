840610f5637dc7927a99d16d6e8b79b8d5537dba
Septian Bekti <septianbekti99@gmail.com>
2023-04-11 12:43:37 +0700

    test

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,11 +2,13 @@
 
 @section('container')
 
+<h1>Halaman Blog Posts</h1>
+
 @foreach ($posts as $post)
 <article class="mb-4">
-    <h2><a href="/posts/{{ ($post["slug"]) }}">{{ $post["title"] }}</a></h2>
-    <h5>By: {{ $post["author"] }}</h5>
-    <p>{{ $post["body"] }}</p>
+    <h2><a href="/posts/{{ $post->id }}">{{ $post->title }}</a></h2>
+    <h5>By: {{ $post->author }}</h5>
+    <p>{{ $post->excerpt }}</p>
 </article>
 @endforeach
 @endsection
